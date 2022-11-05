bd7214711e18a440ccd64c4d3af00221667fa319
Misha <popechitelevm@mail.ru>
2022-11-05 23:17:18 +0400

    refactoring: transition into widgets

--- EM_project/main.cpp
+++ EM_project/main.cpp
@@ -1,34 +1,23 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include "mainwindow.h"
 
+#include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-    QGuiApplication app(argc, argv);
+    QApplication a(argc, argv);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "EM_project_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
-            app.installTranslator(&translator);
+            a.installTranslator(&translator);
             break;
         }
     }
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
